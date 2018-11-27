bc4f5bc2932d7e8041194be7e280e38c25f669ab
Yudai915 <40430090+Yudai915@users.noreply.github.com>
2018-11-28 00:56:09 +0900

    main.cpp fix

--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@
 #include "Other.h"
 #include "SceneManager.h"
 
-#define TITLE 1
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	SetOutApplicationLogValidFlag(false);
@@ -19,35 +17,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	Title_init();
 
-	Player player;
-
-	int player_image = LoadGraph("画像/自機.png");
-	int ballet_image = LoadGraph("画像/たま.png");
-	VECTOR playerpos;
-	VECTOR balletpos;
-
-	playerpos.x = 640;
-	playerpos.y = 500;
-	balletpos.x = 400;
-	balletpos.y = 300;
-
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0)
 	{
-		// 全体の処理に絶対的に必要な処理群
 		Timecount_Update();
 		Keyboard_Update();
 
 		SceneMgr_Update();
 
 		SceneMgr_Draw();
-
-		/*if (((balletpos. < player->Get_pos().x && playerpos.x < balletobj[i]->Get_pos(NULL).x + b_width)
-			|| (playerpos.x < balletobj[i]->Get_pos(NULL).x && balletobj[i]->Get_pos(NULL).x < playerpos.x + p_width))
-			&& ((balletobj[i]->Get_pos(NULL).y < player->Get_pos().y + 10 && player->Get_pos().y + 10 < balletobj[i]->Get_pos(NULL).y + b_height)
-				|| (player->Get_pos().y + 10 < balletobj[i]->Get_pos(NULL).y && balletobj[i]->Get_pos(NULL).y < player->Get_pos().y + p_height))) {
-
-		}
-*/
 	}
 
 	DxLib_End();
